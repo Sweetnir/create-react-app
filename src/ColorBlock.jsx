7e3664cf2539d239ba368dbd1cf8cdcce443ba4e
Damon Herrera <bustinbubs@gmail.com>
2023-05-13 12:49:10 -0700

    first commit

---
+++ src/ColorBlock.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ColorBlock({ color }) {
+    return <div className="ColorBlock"
+    style={ { backgroundColor: color} }>
+
+        <p>{ color }</p>
+    </div>
+}
+
+
+export default ColorBlock;
